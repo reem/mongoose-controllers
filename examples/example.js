9e53d92417e62bdfe7fff0a61d7a91d155d07c75
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-02 11:21:05 -0700

    (feat) Added basic example.

--- examples/example.js
+++ examples/example.js
@@ -1,3 +1,16 @@
 var mongooseControllers = require('mongoose-controllers');
+var express = require('express');
 
-mongooseControllers.doSomethingCool();
+var app = express();
+
+// Magic setup of mongoose
+
+var User = // User Model
+
+app.get('/users', mongooseControllers.get(User, {
+  name: "Default Name"
+}));
+
+app.post('/users/post', mongooseControllers.post(User, {
+  name: 'dataName' // the field in the passed in JSON
+}));
